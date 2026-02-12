d4efe057161ed8a6132099f6cd1988477f43e74c
Mansi Bhat <madhurabhat2310@gmail.com>
2026-02-12 23:14:50 +0530

    Create skills.py

---
+++ Web-Projects/Resume Analyzer/skills.py
@@ -0,0 +1,14 @@
+job_roles = {
+    "software engineer": [
+        "python", "java", "data structures", "algorithms",
+        "sql", "git", "problem solving"
+    ],
+    "data analyst": [
+        "python", "sql", "excel", "data analysis",
+        "statistics", "power bi"
+    ],
+    "ai engineer": [
+        "python", "machine learning", "deep learning",
+        "nlp", "data structures", "git"
+    ]
+}
